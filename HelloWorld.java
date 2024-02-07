9f2129714b6bc9037257f86fbe244fa553c1ca3d
JeusBacani <154034367+JeusBacani@users.noreply.github.com>
2024-02-07 12:34:29 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package prelim.exercise;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am Jeus");
+        System.exit(0);
+
+    }//end of the main method
+}//end of class
